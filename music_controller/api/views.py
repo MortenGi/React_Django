4279a6c6a401728c023962761486d18d0285ed43
Morten Giese <mortengiese@gmx.de>
2022-07-28 19:17:03 +0200

    add comment on querysets

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -5,6 +5,14 @@ from .models import Room
 
 # Create your views here.
 class RoomView(generics.CreateAPIView): #also try: ListAPIView -> only a list, no POST field
-    queryset= Room.objects.all()
-    serializer_class = RoomSerializer
+    #Output html will display "Example" for class ExampleView()
+    queryset= Room.objects.all() #what do we want to return
+    #QUERYSETS are similar to SQL call results 
+    #e.g. 1) Django:   queryset = Room.objects.all().order_by('host').values()
+    #        SQL:      SELECT * FROM Room ORDER BY host;
+    #     2) Django:   queryset = Room.obejcts.values_list('host')
+    #        SQL:      SELECT host FROM Room 
 
+    serializer_class = RoomSerializer #how to convert queryset in some format that I can output
+
+ 
