be63e1bdae039cc8876b36d3c973c9d8cf3a3bcd
Morten Giese <mortengiese@gmx.de>
2022-08-02 16:09:48 +0200

    add some comments

--- music_controller/frontend/src/components/withRouter.js
+++ music_controller/frontend/src/components/withRouter.js
@@ -2,7 +2,12 @@ import React, { Component } from "react";
 
 //this is a hotfix to solve
 //TypeError: Cannot read properties of underfined (reading 'push') at eval (CreateRoomPage.js)
-//TODO look this up
+//useNavigate hook replaces useHistory
+
+//OTHERWISE
+//one can use { withRouter } from 'react-router-dom'; and wrap a component with it like withRouter(comp).
+//comp then has access to this.props.history (if it did not before)
+//inside comp. we then can redirect, hence the name withRouter
 
 import { useNavigate } from "react-router-dom";
 
@@ -10,7 +15,7 @@ export const withRouter = (Component) => {
   const Wrapper = (props) => {
     const navigate = useNavigate();
 
-    return <Component navigate={navigate} {...props} />;
+    return <Component navigate={navigate} {...props} />; //three dots is spread notations
   };
 
   return Wrapper;
